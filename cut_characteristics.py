3dffc7610d16faa172ba71ad9f72b151c39081e5
Brad Hesson <brad.hesson@outlook.com>
2021-08-19 16:57:46 -0600

    improved information from cut_characteristics

--- cut_characteristics.py
+++ cut_characteristics.py
@@ -11,6 +11,10 @@ def main():
         ("X", 0),
         ("Y", 0),
         ("Y", 36),
+        ("Y", 41),
+        ("Y", 64),
+        ("Y", 128),
+        ("Z", 0),
     ]
     for cut in cuts:
         T = cut[1]
@@ -24,12 +28,14 @@ def main():
             np.power(dp[axis, other1 + 3], 2) +
             np.power(dp[axis, other2 + 3], 2)
         )
+        long_perp1 = dp[axis, other1]
+        long_perp2 = dp[axis, other2]
         print(f"{T}° {cut[0]}-Cut:")
-        print(f"     Shear Perp:  {shear}")
-        print(f"     Shear Parr:  {dp[axis, axis+3]}")
-        print(f"    Deform Perp:  {dp[axis,other1]}")
-        print(f"    Deform Perp:  {dp[axis,other2]}")
-        print(f"    Deform Parr:  {dp[axis,axis]}")
+        print(f"|    Shear Perp:  {shear}")
+        print(f"|    Shear Parr:  {dp[axis, axis+3]}")
+        print(f"| Long Max Perp:  {max(long_perp1, long_perp2)}")
+        print(f"| Long Min Perp:  {min(long_perp1, long_perp2)}")
+        print(f"|     Long Parr:  {dp[axis,axis]}")
         print()
 
 
