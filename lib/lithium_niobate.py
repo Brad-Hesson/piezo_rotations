04e056d2dc7cbad4a1f458fa61ec68595cf89a8e
Brad Hesson <brad.hesson@outlook.com>
2021-09-29 14:53:22 -0600

    added type information

--- lib/lithium_niobate.py
+++ lib/lithium_niobate.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 # Piezoelectric constants [pm/V] and matrix (source: https://www.bostonpiezooptics.com/lithium-niobate)
-d15 = 69.2e-12
-d22 = 20.8e-12
-d31 = -0.85e-12
-d33 = 6e-12
-d = np.array(
+d15: float = 69.2e-12
+d22: float = 20.8e-12
+d31: float = -0.85e-12
+d33: float = 6e-12
+d: np.ndarray = np.array(
     [
         [0, 0, 0, 0, d15, -2*d22],
         [-d22, d22, 0, d15, 0, 0],
@@ -15,5 +15,5 @@ d = np.array(
     dtype=np.float64)
 
 # Lattice constants [m] (source: http://www.mt-berlin.com/frames_cryst/descriptions/lnb_lta.htm)
-a = 5.148e-10
-c = 13.863e-10
+a: float = 5.148e-10
+c: float = 13.863e-10
