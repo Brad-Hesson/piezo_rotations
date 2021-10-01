ce2d8d41801079e4780b637a052026626d6bfe4a
Brad Hesson <brad.hesson@outlook.com>
2021-10-01 00:02:36 -0600

    refactor

--- lithium_niobate_planes.py
+++ lithium_niobate_planes.py
@@ -64,38 +64,24 @@ def main():
 
     axis = 0
     angle = 0
+
     rotor = rotors[(axis-1)%3]
     transform = rotor(angle/180*np.pi)
     shear_vec, _, _ = cut_characteristics(d, transform, axis)
+
     E = np.transpose(np.array([0, 0, 0]))
     E[axis] = 1
+
     shear_perp_vec = shear_vec - np.dot(E, shear_vec)
     shear_perp_vec = np.transpose(transform) @ shear_perp_vec
     shear_perp = CVec(*shear_perp_vec)
     shear_lattice_direction = cart_to_lattice(shear_perp)
+
     lattice_vector, err = find_close_lattice_point(shear_lattice_direction, 100)
-    print(" Lattice Vector:", lattice_vector, "+-", err)
     lattice_plane, _ = find_close_lattice_point(lattice_vector.inverted(), 1000)
+
+    print(" Lattice Vector:", lattice_vector, "+-", err)
     print("  Lattice Plane:", lattice_plane)
-
-    return
-    angle = cut['shear_ang'][0]/np.pi*180
-    print(f"Axis Angle: %0.2f degrees from %s towards %s" % (angle, *cut['shear_ang'][1:]))
-
-    cart_vec = CVec(0, 0, 0)
-    cart_vec.y = np.sin(cut['shear_ang'][0])
-    cart_vec.z = np.cos(cut['shear_ang'][0])
-    print("Starting Vector: ", cart_vec)
-
-    lattice = cart_to_lattice(cart_vec)
-    print("Lattice Vector: ", find_close_lattice_point(lattice))
-
-    cvec = lattice_to_cart(lattice)
-    print("Converted Back: ", cvec)
-    angle = np.arctan2(cvec.y, cvec.z)/np.pi*180
-    print(angle)
-    angle = angle if abs(angle) <= 90 else angle % 360-180
-    print(angle)
 
 
 if __name__ == "__main__":
