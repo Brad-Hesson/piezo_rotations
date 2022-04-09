ea34347b213b8c61f72ce85900f6dd311766a5a2
Brad Hesson <brad.hesson@outlook.com>
2022-04-08 23:15:55 -0600

    added a BUNCH of comments for Catlin

--- lib/transformations.py
+++ lib/transformations.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-
+# This is a function that takes a 3d rotation matrix and
+# converts it into a 6-vector rotation matrix as detailed
+# in `Section 7` of `papers/Crystal Properties.pdf`
 def N(a):
     assert(a.shape == (3, 3))
     s0r = [[0, 0, 0], [1, 1, 1], [2, 2, 2]]
@@ -16,15 +18,20 @@ def N(a):
     return np.vstack([np.hstack([na11, na12]), np.hstack([na21, na22])])
 
 
+# Generates a matrix that will rotate about the x-axis by `t` radians
 def Ax(t):
     return np.array([[1, 0, 0], [0, np.cos(t), np.sin(t)], [0, -np.sin(t), np.cos(t)]], dtype=object)
 
 
+# Generates a matrix that will rotate about the y-axis by `t` radians
 def Ay(t):
     return np.array([[np.cos(t), 0, np.sin(t)], [0, 1, 0], [-np.sin(t), 0, np.cos(t)]], dtype=object)
 
 
+# Generates a matrix that will rotate about the z-axis by `t` radians
 def Az(t):
     return np.array([[np.cos(t), np.sin(t), 0], [-np.sin(t), np.cos(t), 0], [0, 0, 1]], dtype=object)
 
+# a list of `rotor` matrix generators, this is useful in other functions
+# that need to programatically generate rotations about different axes
 rotors = [Ax, Ay, Az]
