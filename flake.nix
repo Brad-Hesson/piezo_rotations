dc82f418e70c0b0713a55229756c5a2498e70618
Brad Hesson <brad.hesson@outlook.com>
2024-10-22 13:54:50 -0600

    added flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = flakes: flakes.flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import flakes.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShell = with pkgs; mkShell {
+        packages = [
+          (pkgs.python3.withPackages (ps: [
+            ps.matplotlib
+            ps.numpy
+          ]))
+        ];
+      };
+    }
+  );
+}
+
